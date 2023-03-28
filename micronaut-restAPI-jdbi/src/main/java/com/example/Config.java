c90e535622e69cf786f17b4f73bd15cdadb53e61
pablo gonzalez granados <pagonzal@redhat.com>
2023-03-28 15:15:19 +0200

    Support native compilation on Micronaut-restAPI-jdbi

--- micronaut-restAPI-jdbi/src/main/java/com/example/Config.java
+++ micronaut-restAPI-jdbi/src/main/java/com/example/Config.java
@@ -4,11 +4,117 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Value;
+import io.micronaut.core.annotation.ReflectionConfig;
+import io.micronaut.core.annotation.TypeHint;
 import jakarta.inject.Singleton;
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.config.internal.ConfigCaches;
+import org.jdbi.v3.sqlobject.SqlObjects;
 
 import javax.sql.DataSource;
 
+@ReflectionConfig(
+        type = ConfigCaches.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.core.statement.SqlStatements.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.core.argument.Arguments.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.core.qualifier.Qualifiers.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.core.mapper.RowMappers.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.core.mapper.ColumnMappers.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.core.mapper.Mappers.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.core.collector.JdbiCollectors.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.core.extension.Extensions.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.core.internal.OnDemandExtensions.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.sqlobject.statement.internal.SqlObjectStatementConfiguration.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.sqlobject.Handlers.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.sqlobject.HandlerDecorators.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.sqlobject.statement.internal.SqlQueryHandler.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.core.mapper.reflect.internal.PojoTypes.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = org.jdbi.v3.core.Handles.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
+@ReflectionConfig(
+        type = SqlObjects.class,
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+        }
+)
 @Singleton
 public class Config {
 
@@ -29,7 +135,7 @@ public class Config {
 
     @Factory
     public Jdbi getJdbi(DataSource datasource) {
-        //TODO HikariConfig and / or DataSource are not injected on the context so I have to instantiated manually
+        //TODO HikariConfig and / or DataSource are not injected in the context, so I have to instantiated manually
         var config = new HikariConfig();
         config.setJdbcUrl(datasourceURL);
         config.setUsername(username);
--- micronaut-restAPI-jdbi/src/main/java/com/example/VersionRepository.java
+++ micronaut-restAPI-jdbi/src/main/java/com/example/VersionRepository.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import io.micronaut.core.annotation.ReflectiveAccess;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 
+@ReflectiveAccess
 public interface VersionRepository {
     @SqlQuery("SELECT version()")
     String getVersion();
